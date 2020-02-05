81c0fd17f2799b63dd015bcb72e08c4be9de5450
Jorian Zwerver <jnzwerver@telfort.nl>
2020-02-05 10:52:50 +0000

    hoi

--- JS/H1/H1O19.js
+++ JS/H1/H1O19.js
@@ -20,17 +20,29 @@ function draw() {
 
 function tekenRechthoek(p) {
   push();
+  fill(255,0,0,1);
   strokeWeight(1);
   stroke('green');
-
+  beginShape();
+  vertex(p, 0);
+  vertex(width,height-p);
+  vertex(width-p, height);
+  vertex(0, p);
+  endShape(CLOSE);
   pop();
 }
 
 function tekenVierkant(p) {
   push();
+  fill(255,0,0,1);
   strokeWeight(1);
   stroke('red');
-
+  beginShape();
+  vertex(p, 0);
+  vertex(width,p);
+  vertex(width-p, height);
+  vertex(0, height-p);
+  endShape(CLOSE);
   pop();
 }
 
